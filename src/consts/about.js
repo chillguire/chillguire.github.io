cab5dd7bf140d95703c2bb1341919ae8bc55df81
chillguire <52428156+chillguire@users.noreply.github.com>
2024-10-15 12:40:27 -0400

    add demo user in project desc

--- src/consts/about.js
+++ src/consts/about.js
@@ -4,7 +4,7 @@ export const self = {
 * Systems engineer interested in web development, particularly in the design and implementation of web services. Passionate about learning new technologies and computer science concepts in order to improve my professional and technical self, which along my current knowledge, fuels my thrill to apply them into new and exciting projects.
 * In recent years I have focused my time on working with Node.js on top of different frameworks and libraries for server-side scripting, making use of both SQL and NoSQL databases, nonetheless I also have experience with PHP and Python. Additionally, before focusing on web services I used to be more interested in client-side operations, working with either React and Swift with UIKit in order to develop applications for the end user.
 `,
-	resume: 'https://drive.google.com/file/d/1UfelWHVuRXLLMJSrGk2X_CqorhsVO2M-/view',
+	resume: 'https://drive.google.com/file/d/1MHUJ8bYLMiup8wm3FXzV-bWuxnw57fbo/view',
 	relevantSkills: [
 		'JavaScript',
 		'Python',
@@ -38,7 +38,7 @@ export const self = {
 		{
 			name: 'Standards & Protocols',
 			elements: [
-				'REST APIs', 'HTTP', 'WebSockets', 'OAuth 2.0', 'OpenID Connect', 'JWT',
+				'REST APIs', 'HTTP', 'WebSockets', 'OAuth 2.0', 'OpenID Connect', 'JWT', 'SAML', 'SSO'
 			]
 		},
 		{
